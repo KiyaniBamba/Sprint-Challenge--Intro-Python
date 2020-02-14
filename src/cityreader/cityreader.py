741279afdcf1e10f07c5e624c6e5d43d8f818230
kiyani <bamba.kiyani@gmail.com>
2020-02-14 03:52:14 -0500

    completed cityreader exercice

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,31 +1,51 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+	def __init__(self, name, lat, lon):
+		self.name = name
+		self.lat = lat
+		self.lon = lon
+	
+	def __repr__(self):
+		return f"> City : {self.name}, Latitude: {self.lat}, Lontitude: {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
-# file "cities.csv". (CSV stands for "comma-separated values".)
+# file "cities.csv". (CSV stands for "comma-separated values".) OK
 #
 # In the body of the `cityreader` function, use Python's built-in "csv" module 
-# to read this file so that each record is imported into a City instance. Then
-# return the list with all the City instances from the function.
+# to read this file so that each record is imported into a City instance. OK
+# Then return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
 #
-# Store the instances in the "cities" list, below.
+# Store the instances in the "cities" list, below. OK
 #
 # Note that the first line of the CSV is header that describes the fields--this
-# should not be loaded into a City object.
+# should not be loaded into a City object. OK
+
+import csv
+
 cities = []
+
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
     
+  with open("cities.csv", "r") as csv_file:
+    cities_csv = csv.reader(csv_file)
+    next(cities_csv)
+    for line in cities_csv:
+      cities.append(City(line[0], float(line [3]), float(line[4])))
+      print(cities) 
     return cities
+    
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
+
 for c in cities:
     print(c)
 
